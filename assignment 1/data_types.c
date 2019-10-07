52929a467d8655ec0c1edcdf10857864590c5c0f
say-charlie-1090 <charlie.say.1090@mail.linnbenton.edu>
2019-10-06 19:06:33 -0700

    first assignment

---
+++ assignment 1/data_types.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    //code
+    char ran_char = -5;
+    unsigned char unsigned_char = 'A';
+    int ran_int = 1;
+    signed int sign_int = -1;
+    unsigned int unsign_int = 2;
+    short int short_int = -97;
+    unsigned short int unsign_short_int = 97;
+    long int long_int = -2343563;
+    unsigned long int unsign_long_int = 2345637;
+    float my_float = 3.145;
+    double my_double = 3.14;
+    long double long_double = 3.321354;
+    
+    printf("My char is: %d\n", ran_char);
+    printf("My unsigned char is: %d\n", unsigned_char);
+    printf("My integer is: %d\n", ran_int);
+    printf("My signed integer is: %d\n", sign_int);
+    printf("My unsigned integer is: %d\n", unsign_int);
+    printf("My short integer is: %d\n", short_int);
+    printf("My unsigned short integer is: %d\n", unsign_short_int);
+    printf("My long integer is: %d\n", long_int);
+    printf("My unsigned long integer is: %d\n", unsign_long_int);
+    printf("My float is: %d\n", my_float);
+    printf("My double is: %d\n", my_double);
+    printf("My long double is: %d\n", long_double);
+
+
+    return 0;
+}
