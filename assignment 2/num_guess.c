071da2e234814a9874dd35738eefea6c69779d3b
charlie-say <charliesay1990@hotmail.com>
2019-10-14 13:14:05 -0700

    play again

--- assignment 2/num_guess.c
+++ assignment 2/num_guess.c
@@ -1,49 +1,70 @@
 #include <stdio.h>
-
-//Ask one user to enter a secret number to be guessed
-
+#include <string.h> //found at https://cboard.cprogramming.com/c-programming/48313-if-statements-using-strings-condition.html
 
 int main()
 {
-    int low;
-    low = 1;
-    int high;
-    high = 100;
+  int low;
+  low = 1;
+  int high;
+  high = 100;
 
-    int secret_num;
-    printf("Enter a secret number: ");
-    scanf("%d", &secret_num);
-    printf("You entered: %d\n", secret_num);
+  int secret_num;
+  printf("Enter a secret number: ");
+  scanf("%d", &secret_num);
+  printf("You entered: %d\n", secret_num);
 
-    int i;
-    for (i=1; i<=5; i++)
+  int i;
+  for (i = 1; i <= 5; i++)
+  {
+
+    int num_guess;
+
+    printf("Guess the secret number between the range of %d - %d: ", low, high);
+    scanf("%d", &num_guess);
+
+    if (num_guess < low || num_guess > high)
     {
-
-      int num_guess;
-
-      printf("Guess the secret number between the range of %d - %d: ", low, high);
-      scanf("%d", &num_guess);
-
-      if (num_guess == secret_num)
-      {
-        printf("Congratulations!");
-        return 0;
-      }
-
-      if (num_guess < secret_num)
-      {
-        printf("Your guess is too low.\n");
-        low = num_guess + 1;
-      }
-
-      if (num_guess > secret_num)
-      {
-        printf("Your guess is too high.\n");
-        high = num_guess - 1;
-      }
-
+      printf("Your guess is out of range. Try again.\n");
+      continue;
     }
-    
+
+    if (num_guess == secret_num)
+    {
+      printf("Congratulations!");
+      return 0;
+    }
+
+    if (num_guess < secret_num)
+    {
+      printf("Your guess is too low.\n");
+      low = num_guess + 1;
+    }
+
+    if (num_guess > secret_num)
+    {
+      printf("Your guess is too high.\n");
+      high = num_guess - 1;
+    }
+  }
+
+  return 0;
+}
+
+void play_again()
+{
+  char user_input;
+
+  printf("GAME OVER. Play again? y/n: ");
+  scanf("%c", &user_input);
+
+  if (strcmp(&user_input, "y") == 0)
+  {
+    main();
+  }
+  else
+  {
+    printf("Thanks for playing!");
+  }
 }
 
 //-accept and stores secret number into a variable
